b86b3026273acb7390fe700f5521826c777f3060
zhouchunxiu <chunxiu_z@163.com>
2019-05-26 16:21:44 +0800

    add main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,23 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+    var arr = [['._.','...','._.','._.','...','._.','._.','._.','._.','._.'],
+               ['|.|','..|','._|','._|','|_|','|_.','|_.','..|','|_|','|_|'],
+               ['|_|','..|','|_.','._|','..|','._|','|_|','..|','|_|','..|']];
+    var numarr = [];
+    while(num > 0){
+        numarr.unshift(num%10);
+        num /= 10;
+    }
+    var resultarr = [];
+    var result = [];
+    var index = 0;
+    while(index++ < 3){
+        for(var i = 0; i < numarr.length; i++){
+            resultarr.push(arr[index][num[i]]);
+        }
+        result.push(resultarr.join(' ')+'\n');
+        resultarr = [];
+    }
+
+    //console.log("Debug Info");
+    return result;
 };
