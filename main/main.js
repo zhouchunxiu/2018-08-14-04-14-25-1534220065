18b52cdc2bd3fc7b930dab719036a142518b99f1
zhouchunxiu <chunxiu_z@163.com>
2019-05-26 17:21:59 +0800

    parseInt num

--- main/main.js
+++ main/main.js
@@ -5,10 +5,10 @@ module.exports = function main(num) {
     var numarr = [];
     while(num > 0){
         numarr.push(num%10);
-        num /= 10;
+        num = parseInt(num / 10);
     }
     numarr.reverse();
-    console.log("numarr:" + numarr);
+    console.log("numarr = " + numarr);
     var resultarr = [[],[],[]];
     var result = [];
     var index = 0;
@@ -20,6 +20,6 @@ module.exports = function main(num) {
         index++;
     }
     var text = result[0] + "\n" + result[1] + "\n" + result[2];
-
+    console.log(result);
     return text;
 };
